1a5e51afb0587ba268aa5cf3dcd9936ece3e2788
rynslmns <ryansalmons@utexas.edu>
2020-11-01 22:16:38 -0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Burger Boys`,
+    description: `We review every burger in the great Los Angeles area.`,
+    author: `Ryan Salmons`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi nerds</h1>
+    <p>We are going to review every burger in LA.</p>
+    <p>Under construction.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
